243f4a7d727807b0f09af7ab74bef278525d2f65
dimon4uk1990 <mexal07@ukr.net>
2022-07-24 20:01:48 +0300

    2 завдання, функція запису

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
-import { throttle } from 'lodash.throttle';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+const LOCALSTORAGE_KEY = 'videoplayer-current-time';
+console.log(player);
+
+player.on('timeupdate', throttle(recordsEimePlayed, 1000));
+
+function recordsEimePlayed({ seconds }) {
+  localStorage.setItem(LOCALSTORAGE_KEY, seconds);
+  console.log('rere');
+}
